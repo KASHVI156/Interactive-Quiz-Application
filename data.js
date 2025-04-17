9f550faf3a44df95239c3b9bffe4972dd34fa77b
HITAKSHI <hitakshi0349.becse24@chitkara.edu.in>
2025-04-17 14:33:28 +0530

    Data.js file updated

---
+++ data.js
@@ -0,0 +1,51 @@
+// Quiz Categories
+const categories = [
+
+    {
+
+        id: 1,
+
+        name: "Mathematics",
+
+        slug: "math",
+
+        icon: "✖️",
+        color: "bg-blue",
+
+        questionCount: 10
+
+    },
+
+    {
+
+        id: 2,
+
+        name: "Science",
+
+        slug: "science",
+
+        icon: "🧪",
+        color: "bg-green",
+
+        questionCount: 15
+
+    },
+
+    {
+
+        id: 3,
+
+        name: "General Knowledge",
+
+        slug: "general",
+
+        icon: "🌍",
+        color: "bg-purple",
+
+        questionCount: 12
+
+    }
+
+];
+
+
