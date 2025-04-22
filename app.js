55edcbff46d63bdf2287b1b2b309b018c3651932
khushi131313 <khannakhushi285@gmail.com>
2025-04-22 10:09:40 +0530

    work is done

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const homePage = document.getElementById('home-page');
 const quizIntroPage = document.getElementById('quiz-intro-page');
 const quizPage = document.getElementById('quiz-page');
 const resultsPage = document.getElementById('results-page');
+// Navigation Elements
+const homeLink = document.getElementById('home-link');
+const backToHomeBtn = document.getElementById('back-to-home-btn');
+const startQuizBtn = document.getElementById('start-quiz-btn');
+const quitQuizBtn = document.getElementById('quit-quiz-btn');
+const submitBtn = document.getElementById('submit-btn');
+const nextBtn = document.getElementById('next-btn');
+const tryAgainBtn = document.getElementById('try-again-btn');
+const backHomeBtn = document.getElementById('back-home-btn');
+
