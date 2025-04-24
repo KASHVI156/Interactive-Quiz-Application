fa19ba37d5ccd62e475f8ea745f8848aef76105b
khushi131313 <khannakhushi285@gmail.com>
2025-04-24 08:04:46 +0530

    7

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // DOM Elements.......................
-// Page Elements
+// Page Elements,,
 const homePage = document.getElementById('home-page');
 const quizIntroPage = document.getElementById('quiz-intro-page');
 const quizPage = document.getElementById('quiz-page');
